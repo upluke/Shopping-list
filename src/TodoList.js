9e9a725ab89d2a29902187dc8df8ff296bbd6ab8
upluke <lukeforadream@gmail.com>
2020-11-30 21:50:32 -0800

    fixKeyError

--- src/TodoList.js
+++ src/TodoList.js
@@ -10,19 +10,19 @@ export default function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
       <Paper>
         <List>
           {todos.map((todo, index) => (
-            <>
+            <div key={todo.id}>
               <Todo
                 {...todo}
                 // id={todo.id}
                 // task={todo.task}
-                key={todo.id}
+
                 // completed={todo.completed}
                 removeTodo={removeTodo}
                 toggleTodo={toggleTodo}
                 editTodo={editTodo}
               />
               {index < todos.length - 1 && <Divider />}
-            </>
+            </div>
           ))}
         </List>
       </Paper>
